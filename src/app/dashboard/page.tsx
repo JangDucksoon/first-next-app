e642160f35b4175c270f739c971e038aadfb7fbe
jds <smaa11271@wavus.co.kr>
2025-07-30 12:37:24 +0900

    dashboard title cursor default 추가

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
                     <div className="flex h-90 min-h-0 flex-col rounded-lg border border-gray-200 p-3">
                         <div className="flex w-full items-center border-b border-b-gray-200">
                             <Tooltip style="dark" content="표준 사전 카테고리 분포 파이 차트">
-                                <h3 className="pb-3 text-2xl font-bold">Category Distribution of Dictionary Entries (PIE)</h3>
+                                <h3 className="cursor-default pb-3 text-2xl font-bold">Category Distribution of Dictionary Entries (PIE)</h3>
                             </Tooltip>
                         </div>
                         <div className="flex w-full flex-1 pt-3">
@@ -35,7 +35,7 @@ export default function Page() {
                     <div className="flex h-90 min-h-0 flex-col rounded-lg border border-gray-200 p-3">
                         <div className="flex w-full items-center border-b border-b-gray-200">
                             <Tooltip style="dark" content="표준 사전 카테고리 분포 레이더 차트">
-                                <h3 className="pb-3 text-2xl font-bold">Category Distribution of Dictionary Entries (Radar)</h3>
+                                <h3 className="cursor-default pb-3 text-2xl font-bold">Category Distribution of Dictionary Entries (Radar)</h3>
                             </Tooltip>
                         </div>
                         <div className="flex w-full flex-1 pt-3">
@@ -47,7 +47,7 @@ export default function Page() {
                     <div className="flex h-90 min-h-0 flex-col rounded-lg border border-gray-200 p-3">
                         <div className="flex w-full items-center border-b border-b-gray-200">
                             <Tooltip style="dark" content="표준 사전 카테고리 분포 트리맵">
-                                <h3 className="pb-3 text-2xl font-bold">Category Distribution of Dictionary Entries (Tree)</h3>
+                                <h3 className="cursor-default pb-3 text-2xl font-bold">Category Distribution of Dictionary Entries (Tree)</h3>
                             </Tooltip>
                         </div>
                         <div className="flex w-full flex-1 pt-3">
@@ -59,7 +59,7 @@ export default function Page() {
                     <div className="flex h-90 min-h-0 flex-col rounded-lg border border-gray-200 p-3">
                         <div className="flex w-full items-center border-b border-b-gray-200">
                             <Tooltip style="dark" content="표준 단어 형식단어여부별 비율">
-                                <h3 className="pb-3 text-2xl font-bold">Distribution of Formality in Standard Words</h3>
+                                <h3 className="cursor-default pb-3 text-2xl font-bold">Distribution of Formality in Standard Words</h3>
                             </Tooltip>
                         </div>
                         <div className="flex w-full flex-1 pt-3">
@@ -71,7 +71,7 @@ export default function Page() {
                     <div className="flex h-90 min-h-0 flex-col rounded-lg border border-gray-200 p-3">
                         <div className="flex w-full items-center border-b border-b-gray-200">
                             <Tooltip style="dark" content="표준 도메인 대분류별 분포">
-                                <h3 className="pb-3 text-2xl font-bold">Standard Domain Group Distribution</h3>
+                                <h3 className="cursor-default pb-3 text-2xl font-bold">Standard Domain Group Distribution</h3>
                             </Tooltip>
                         </div>
                         <div className="flex w-full flex-1 pt-3">
@@ -83,7 +83,7 @@ export default function Page() {
                     <div className="flex h-90 min-h-0 flex-col rounded-lg border border-gray-200 p-3">
                         <div className="flex w-full items-center border-b border-b-gray-200">
                             <Tooltip style="dark" content="표준 용어 참조 단어 개수">
-                                <h3 className="pb-3 text-2xl font-bold">Standard Term Reference Word Count</h3>
+                                <h3 className="cursor-default pb-3 text-2xl font-bold">Standard Term Reference Word Count</h3>
                             </Tooltip>
                         </div>
                         <div className="flex w-full flex-1 pt-3">
